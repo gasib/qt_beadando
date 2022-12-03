65abfd23de3439815f82a6ac09d30267c2580b23
Brendon Márk Gadácsi <f2zu17@INF.ELTE.HU>
2022-12-03 19:43:33 +0100

    Added basic gomoku model logic

--- Gomoku/gomokumodel.cpp
+++ Gomoku/gomokumodel.cpp
@@ -5,3 +5,94 @@ GomokuModel::GomokuModel(QObject *parent)
 {
 
 }
+
+void GomokuModel::checkTable(int x, int y)
+{
+    if (!canBeChecked(x,y))
+        return;
+
+    _table[x][y] = _currentPlayer;
+    emit tableChanged(x, y, _currentPlayer);
+    if (isAllChecked())
+    {
+        emit gameOver();
+        return;
+    }
+    if (maxNeighboring(x, y) >= 5)
+    {
+        gameWon(_currentPlayer);
+        return;
+    }
+    _currentPlayer = _currentPlayer == Player::X ? Player::O : Player::X;
+}
+
+int GomokuModel::getSize() const
+{
+    return _tableSize;
+}
+
+void GomokuModel::initTable(int size)
+{
+    _currentPlayer = Player::X;
+    _maxNeighborPlayerX = 0;
+    _maxNeighborPlayerO = 0;
+    _tableSize = size;
+    _table.clear();
+    for (int i = 0; i < size; ++i)
+    {
+        QVector<Player> newRow;
+        for (int j = 0; j < size; ++j)
+        {
+            newRow.append(Player::None);
+        }
+        _table.append(newRow);
+    }
+    emit tableInitialized();
+}
+
+bool GomokuModel::canBeChecked(int x, int y)
+{
+    if (x < 0 || x >= _tableSize || y < 0 || y >= _tableSize || _table[x][y] != Player::None)
+    {
+        emit invalidPosition(x, y);
+        return false;
+    }
+    return true;
+}
+
+int GomokuModel::maxNeighboring(int x, int y)
+{
+    return std::max({checkInDirection(x+1, y, 1, 0, 1, _table[x][y]),
+                     checkInDirection(x-1, y, -1, 0, 1, _table[x][y]),
+                     checkInDirection(x, y+1, 0, 1, 1, _table[x][y]),
+                     checkInDirection(x, y-1, 0, -1, 1, _table[x][y]),
+                     checkInDirection(x+1, y-1, 1, -1, 1, _table[x][y]),
+                     checkInDirection(x-1, y-1, -1, -1, 1, _table[x][y]),
+                     checkInDirection(x+1, y+1, 1, 1, 1, _table[x][y]),
+                     checkInDirection(x-1, y+1, -1, 1, 1, _table[x][y])
+                   });
+}
+
+int GomokuModel::checkInDirection(int x, int y, int dirx, int diry, int current, Player player)
+{
+    if (x < 0 || x >= _tableSize || y < 0 || y >= _tableSize)
+        return current;
+
+    if (_table[x][y] == player)
+        return checkInDirection(x+dirx, y+diry, dirx, diry, current+1, player);
+
+    return current;
+}
+
+bool GomokuModel::isAllChecked()
+{
+    for (int i = 0; i < _tableSize; ++i)
+    {
+        for (int j = 0; j < _tableSize; ++j)
+        {
+            if (_table[i][j] == Player::None)
+                return false;
+        }
+    }
+    return true;
+}
--- Gomoku/gomokumodel.h
+++ Gomoku/gomokumodel.h
@@ -1,7 +1,14 @@
 #ifndef GOMOKUMODEL_H
 #define GOMOKUMODEL_H
 
+#include "player.h"
+
 #include <QObject>
+#include <QVector>
+
+enum class Direction {
+    Top, TopRight, Right, BottomRight, Bottom, BottomLeft, Left, TopLeft
+};
 
 class GomokuModel : public QObject
 {
@@ -9,8 +16,34 @@ class GomokuModel : public QObject
 public:
     explicit GomokuModel(QObject *parent = nullptr);
 
+    void checkTable(int x, int y);
+    int getSize() const;
+    void initTable(int size);
+
 signals:
 
+    void tableInitialized();
+    void tableChanged(int x, int y, Player player);
+    void invalidPosition(int x, int y);
+    void gameWon(Player player);
+    void gameOver();
+
+private:
+
+    bool canBeChecked(int x, int y);
+    // Must be call before changing player
+    int maxNeighboring(int x, int y);
+    int checkInDirection(int x, int y, int dirx, int diry, int current, Player player);
+    bool isAllChecked();
+
+private:
+
+    QVector<QVector<Player>> _table;
+    Player _currentPlayer;
+    int _tableSize;
+
+    int _maxNeighborPlayerX;
+    int _maxNeighborPlayerO;
 };
 
 #endif // GOMOKUMODEL_H
